b6c56dac7ba3482a946f4bba8cb86901fddafc07
lw-michael <lw-michael@lw-michael-thinkpad>
2013-08-05 13:54:55 +0200

    initial commit

--- agent_updater.class.php
+++ agent_updater.class.php
@@ -23,18 +23,6 @@ class agent_updater extends lw_agent
 
     protected function showEdit()
     {
-        #$xml = simplexml_load_file($this->config["path"]["resource"] . "test.xml");
-        #die($xml->Kategorie[0]->Name);
-        #die($xml->name[0]["id"]);
-        #die($xml->test[0]->name->attributes()->id."");
-        #die($xml->test[0]->content);
-        #die($xml->file[0]->md5);
-        #foreach($xml->file as $file){
-        #    $str.= $file->path."<br>";
-        #}
-        #die($str);
-        #print_r($array);
-        #die("HIER");
         $response = new \AgentUpdater\Services\Response();
         $controller = new \AgentUpdater\Controller\UpdaterController($this->config, $response, $this->request);
         $controller->execute();
